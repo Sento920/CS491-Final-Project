7d1c6a121c93984cd5ce84f4fa0620d3eb80ee48
uwecrayat <rayat@uwec.edu>
2015-10-26 10:44:36 -0500

    moved things to folders

---
+++ Mechanics testing/Assets/Scripts/LineRendererCode.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LineRendererCode : MonoBehaviour {
+	
+	//Line Renderer
+	public Texture mySprite;
+	public Vector3 myPoint1;
+	public Vector3 myPoint2;
+	
+	//Material
+	public Material myMaterial;
+	public float xScaleFactor;
+	private float adjustedXSize;
+	
+	// Update is called once per frame
+	void Update () {
+		myMaterial.SetTexture(0, mySprite);
+		GetComponent<LineRenderer> ().SetPosition (0, myPoint1);
+		GetComponent<LineRenderer> ().SetPosition (1, myPoint2);
+		adjustedXSize = xScaleFactor * (Mathf.Abs(Vector2.Distance(myPoint1, myPoint2)));
+		
+		myMaterial.mainTextureScale = new Vector2(adjustedXSize, myMaterial.mainTextureScale.y);
+	}
+}
---
+++ Mechanics testing/Assets/Scripts/Test.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+	public float XScale;
+	public float YScale;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.GetComponent<Renderer>().material.mainTextureScale = new Vector2(XScale , YScale );
+
+	}
+}
