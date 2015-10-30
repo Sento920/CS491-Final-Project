7133397d4a403c244d3589c39fb0a181d014b947
uwecrayat <rayat@uwec.edu>
2015-10-30 13:03:20 -0500

    Made a quick grappling hook sprite and made the rope interpolate towards its target

--- Mechanics testing/Assets/Scripts/PlayerController.cs
+++ Mechanics testing/Assets/Scripts/PlayerController.cs
@@ -4,28 +4,35 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 	private float dist;
 	private Vector3 mousePos;
+	private float newX;
+	private float newY;
+	private Rigidbody2D rb2D;
 	// Use this for initialization
 	void Start () {
-	
+//		GetComponentInChildren<LineRendererCode>().myPoint1 = transform.position;
+		rb2D = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float input = Input.GetAxis ("Horizontal");
-		GetComponent<Rigidbody2D> ().AddForce (new Vector2 (input * 5, 0f));
-
+		GetComponentInChildren<LineRendererCode>().myPoint2 = Vector2.Lerp(mousePos, transform.position, dist);
+		transform.rotation = Quaternion.LookRotation (Vector3.forward, mousePos - transform.position);
 		if (Input.GetMouseButton (0)) {
-			GetComponent<DistanceJoint2D> ().enabled = true;
+
+			dist = Vector3.Distance (transform.position, mousePos);
+
+//			GetComponent<DistanceJoint2D> ().enabled = true;
 			mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 			mousePos.z = 0f;
+
+
 			GetComponent<DistanceJoint2D> ().connectedAnchor = mousePos;
-			dist = Vector3.Distance (transform.position, mousePos);
 			GetComponentInChildren<LineRendererCode>().myPoint1 = transform.position;
-			GetComponentInChildren<LineRendererCode>().myPoint2 = mousePos;
+
+
 //			print (Vector3.Distance(transform.position, mousePos));
 		} else {
-			GetComponent<DistanceJoint2D> ().enabled = false;
+//			GetComponent<DistanceJoint2D> ().enabled = false;
 		}
 	}
-
 }
