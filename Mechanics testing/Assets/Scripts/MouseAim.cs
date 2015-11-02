00df43947c98c662dea172fe86235768b7a486e3
uwecrayat <rayat@uwec.edu>
2015-11-02 13:26:37 -0600

    Added a quick aiming line graphic and added script to angle the graphic toward the mouse

---
+++ Mechanics testing/Assets/Scripts/MouseAim.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseAim : MonoBehaviour {
+	private Vector3 mousePos;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		float angle = Mathf.Atan2 (mousePos.y-transform.position.y, mousePos.x-transform.position.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.AngleAxis (angle, Vector3.forward);	
+	}
+}
