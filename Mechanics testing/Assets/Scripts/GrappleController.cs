9c82aa5aad8cb84346c896c3a90e812fdf0cd3be
uwecrayat <rayat@uwec.edu>
2015-11-05 13:26:53 -0600

    Reworked grapple script. Aim with mouse and fire with space bar. Distance joint seems to stop working when velocity is zero. Not sure how to fix yet

--- Mechanics testing/Assets/Scripts/GrappleController.cs
+++ Mechanics testing/Assets/Scripts/GrappleController.cs
@@ -1,42 +1,62 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GrappleController : MonoBehaviour {
+public class GrappleController : MonoBehaviour
+{
 	private float dist;
-	private Vector3 mousePos;
+	private Vector3 hitPos;
 	private float newX;
 	private float newY;
 	private Rigidbody2D rb2D;
 	private LineRendererCode lrc;
+	private DistanceJoint2D dj2D;
+	public Transform parent;
+	private Vector3 parentPos;
+	private bool fired;
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 //		GetComponentInChildren<LineRendererCode>().myPoint1 = transform.position;
-		rb2D = GetComponent<Rigidbody2D> ();
+		parentPos = parent.position;
 		lrc = GetComponentInChildren<LineRendererCode> ();
+		lrc.myPoint1 = parentPos;
+		rb2D = GetComponent<Rigidbody2D> ();
+		dj2D = GetComponentInParent<DistanceJoint2D> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
+		parentPos = parent.position;
 
-//		lrc.myPoint2 = Vector2.Lerp(mousePos, transform.position, dist);
-				lrc.myPoint2 = Vector2.Lerp(transform.position, mousePos, dist);
-		transform.rotation = Quaternion.LookRotation (Vector3.forward, mousePos - transform.position);
-		if (Input.GetMouseButton (0)) {
-
-			dist = Vector3.Distance (transform.position, mousePos);
-
-//			GetComponent<DistanceJoint2D> ().enabled = true;
-			mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-			mousePos.z = 0f;
-
-
-			GetComponent<DistanceJoint2D> ().connectedAnchor = mousePos;
-			lrc.myPoint1 = transform.position;
-
-
-//			print (Vector3.Distance(transform.position, mousePos));
-		} else {
-//			GetComponent<DistanceJoint2D> ().enabled = false;
+		print (parentPos);
+		print (transform.position);
+		dist = Vector3.Distance (parentPos, hitPos);
+		lrc.myPoint1 = parentPos;
+		lrc.myPoint2 = transform.position;
+		if (!fired) {
+			hitPos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			hitPos.z = 0f;
+			transform.position = parentPos;
+			transform.rotation = Quaternion.LookRotation (Vector3.forward, hitPos - transform.position);
 		}
+
+		if (Input.GetKeyDown (KeyCode.Space) && !fired) {
+			fired = true;
+			rb2D.AddForce (transform.up * 1000);
+		}
+		if (fired) {
+			dj2D.distance -= Input.GetAxis("Vertical")/2f;
+		}
+	}
+
+	void OnCollisionEnter2D (Collision2D coll)
+	{
+		hitPos = coll.contacts [0].point;
+		transform.position = hitPos;
+		rb2D.constraints = RigidbodyConstraints2D.FreezePosition;
+		dj2D.connectedAnchor = hitPos;
+		dj2D.enabled = true;
+		dj2D.distance = dist;
 	}
 }
