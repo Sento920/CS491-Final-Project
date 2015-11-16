cdefa9fbf60875fa0a2cdb2508149c48f62b21ee
uwecrayat <rayat@uwec.edu>
2015-11-16 12:38:45 -0600

    Removed left/right walk animations and went back to a single walk animation using local X scale to flip. Fixed aiming when flipped. Still need to fix grapple head rotation when flipped

--- Mechanics testing/Assets/Scripts/PlayerController.cs
+++ Mechanics testing/Assets/Scripts/PlayerController.cs
@@ -4,26 +4,40 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 	private Rigidbody2D rb2D;
 	private Animator animator;
+	private MouseAim mouseAim;
+	private bool isGrounded;
+	private bool direction;
 	// Use this for initialization
 	void Start () {
 		rb2D = GetComponent<Rigidbody2D> ();
 		animator = GetComponent<Animator> ();
-		animator.SetBool("isGrounded", true);
-
+		mouseAim = GetComponentInChildren<MouseAim> ();
+		isGrounded = true;
+		direction = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		float push = Input.GetAxis ("Horizontal");
-		if (push != 0) {
+		if (isGrounded && push != 0) {
 			rb2D.velocity=(new Vector2(push * 5, 0));
+			animator.SetBool("isGrounded", true);
+			animator.SetBool("Walk", true);
 			if (push < 0) {
-				animator.SetInteger("WalkDir", -1);
+				direction = false;
+				mouseAim.dir = false;
 			} else {
-				animator.SetInteger("WalkDir", 1);
+				direction = true;
+				mouseAim.dir = true;
 			}
 		} else {
-			animator.SetInteger("WalkDir", 0);
+			animator.SetBool("Walk", false);
+		}
+
+		if (direction) {
+			transform.localScale = new Vector2 (1, 1);
+		} else {
+			transform.localScale = new Vector2 (-1, 1);
+		}
 		}
 	}
-}
