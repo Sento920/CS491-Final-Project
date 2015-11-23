0814260cbcd24b4ce82f635c54a66e7c7dfee9d5
uwecrayat <rayat@uwec.edu>
2015-11-23 10:57:40 -0600

    Left click fires and releases grapple. Grapple hanging max distance limited to public grappleMaxDist

--- Mechanics testing/Assets/Scripts/GrappleController.cs
+++ Mechanics testing/Assets/Scripts/GrappleController.cs
@@ -11,17 +11,19 @@ public class GrappleController : MonoBehaviour
 	private LineRendererCode lrc;
 	private SpringJoint2D sj2D;
 	public Transform parent;
-	private Vector3 parentPos;
+	private Vector3 playerPos;
 	private bool fired;
 	private bool grappleHit;
+	public float grappleMaxDist;
 	private Vector3 mousePos;
 	// Use this for initialization
 	void Start ()
 	{
-//		GetComponentInChildren<LineRendererCode>().myPoint1 = transform.position;
-		parentPos = parent.position;
+		//		GetComponentInChildren<LineRendererCode>().myPoint1 = transform.position;
+		playerPos = parent.position;
 		lrc = GetComponentInChildren<LineRendererCode> ();
-		lrc.myPoint1 = parentPos;
+		lrc.enabled = false;
+		lrc.myPoint1 = playerPos;
 		rb2D = GetComponent<Rigidbody2D> ();
 		sj2D = GetComponentInParent<SpringJoint2D> ();
 	}
@@ -29,43 +31,55 @@ public class GrappleController : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		print (transform.up);
-		parentPos = parent.position;
-
-
-		lrc.myPoint1 = parentPos;
+		playerPos = parent.position;
+		
+		
+		lrc.myPoint1 = playerPos;
 		lrc.myPoint2 = transform.position;
+
 		if (!fired) {
 			mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 			mousePos.z = 0f;
-			transform.position = parentPos;
+			transform.position = playerPos;
 			transform.rotation = Quaternion.LookRotation (Vector3.forward, mousePos - transform.position);
 		}
 
-		if (Input.GetKeyDown (KeyCode.Space) && !fired) {
+		//fire grapple
+		if (Input.GetMouseButtonDown(0) && !fired) {
 			fired = true;
+			lrc.enabled = true;
+			GetComponentInChildren<LineRenderer> ().enabled = true;
 			GetComponent<SpriteRenderer>().enabled = true;
-			rb2D.AddForce (transform.up * 1000);
+			rb2D.AddForce (transform.up * 1500);
 		} 
+		//move up/down rope
 		if (grappleHit) {
-			sj2D.distance -= Input.GetAxis("Vertical")/4f;
+			sj2D.distance = Mathf.Clamp(sj2D.distance - Input.GetAxis("Vertical")/4f, 0, grappleMaxDist);
 		}
-		if (Input.GetKeyDown (KeyCode.LeftShift) && grappleHit) {
-			sj2D.enabled = false;
-			GetComponent<SpriteRenderer>().enabled = false;
-			rb2D.constraints = RigidbodyConstraints2D.None;
-			grappleHit = false;
-			fired = false;
+		//release grapple
+		if (Input.GetMouseButtonDown(0) && grappleHit) {
+			ResetGrapple();
 		}
 	}
 
+	void ResetGrapple() {
+		sj2D.enabled = false;
+		lrc.enabled = true;
+		GetComponentInChildren<LineRenderer> ().enabled = false;
+		GetComponent<SpriteRenderer>().enabled = false;
+		rb2D.constraints = RigidbodyConstraints2D.None;
+		grappleHit = false;
+		fired = false;
+	}
+	
 	void OnCollisionEnter2D (Collision2D coll)
 	{
 		grappleHit = true;
 		hitPos = coll.contacts [0].point;
-		dist = Vector3.Distance (parentPos, hitPos);
+		dist = Vector3.Distance (playerPos, hitPos);
 		transform.position = hitPos;
-		rb2D.constraints = RigidbodyConstraints2D.FreezePosition;
+		rb2D.transform.rotation = Quaternion.identity;
+		rb2D.constraints = RigidbodyConstraints2D.FreezeAll;
 		sj2D.connectedAnchor = hitPos;
 		sj2D.distance = dist;
 		sj2D.enabled = true;
