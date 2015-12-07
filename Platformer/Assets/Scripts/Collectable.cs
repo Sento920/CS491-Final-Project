aa0b601d36b747dbc1688982e7edc7203ec84fe8
Sento920 <ruschta@uwec.edu>
2015-12-07 10:15:17 -0600

    Added Complete Level design for Plains area.

---
+++ Platformer/Assets/Scripts/Collectable.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collectable : MonoBehaviour {
+
+    public GameObject Master;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (CompareTag("Player"))
+        {
+            //Add score.
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Platformer/Assets/Scripts/ScoreControl.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreControl : MonoBehaviour {
+
+    public static int CoinsCollected;
+    public static int Score;
+    public Text ScoreText;
+    public Text CoinText;
+
+    // Use this for initialization
+    void Start () {
+        CoinsCollected = 0;
+        Score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    //Update Score text
+	}
+
+    static void CoinCollected()
+    {
+        CoinsCollected++;
+    }
+
+    static void EnemyDestroyed(int value)
+    {
+        Score += value;
+    }
+
+    void UpdateScore()
+    {
+        ScoreText.text = ("Score: " + Score);
+        CoinText.text = ("Coins: " + CoinsCollected);
+    }
+}
