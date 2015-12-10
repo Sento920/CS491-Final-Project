5afeb73e3bdb1b1959886424f65be4b4df77c2bc
uwecrayat <rayat@uwec.edu>
2015-12-10 16:35:09 -0600

    Created a block material fix script to tile square textures correctly. The water and any other tiles we may use will need to be square. The water is currently 128x32 and needs to be made 32x32.

---
+++ Platformer/Assets/Scripts/BlockMaterialFix.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockMaterialFix : MonoBehaviour {
+
+    private Material instancedMaterial;
+	// Use this for initialization
+	void Start () {
+        instancedMaterial = new Material(GetComponent<Renderer>().material);
+        instancedMaterial.mainTextureScale = new Vector2(-transform.localScale.x, -transform.localScale.y);
+        GetComponent<Renderer>().material = instancedMaterial;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
