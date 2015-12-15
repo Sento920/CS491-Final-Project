34632d70aa68bb5bff46bb11702ccbc1fd38a998
uwecrayat <rayat@uwec.edu>
2015-12-15 13:58:59 -0600

    Background vertical shifting cleaned up

--- Platformer/Assets/Scripts/ParallaxBackground.cs
+++ Platformer/Assets/Scripts/ParallaxBackground.cs
@@ -4,12 +4,16 @@ using System.Collections;
 public class ParallaxBackground : MonoBehaviour {
     public float lvlEdgeToCameraEdge;
     public float BGEdgeToCameraEdge;
+	public float lvlCeilToCameraEdge;
+	public float BGCeilToCameraEdge;
     private Vector3 startPos;
     private Vector3 bgStartPos;
-    private float bgShift;
+    private float bgHorizShift;
+	private float bgVertShift;
 	// Use this for initialization
 	void Start () {
-        bgShift = BGEdgeToCameraEdge / lvlEdgeToCameraEdge;
+		bgHorizShift = BGEdgeToCameraEdge / lvlEdgeToCameraEdge;
+		bgVertShift = BGCeilToCameraEdge / lvlCeilToCameraEdge;
         startPos = Camera.main.transform.position;
         bgStartPos = transform.position;
 
@@ -19,10 +23,10 @@ public class ParallaxBackground : MonoBehaviour {
 	void LateUpdate () {
         float distMovedX = Mathf.Abs(Camera.main.transform.position.x - startPos.x);
         float distMovedY = Mathf.Abs(Camera.main.transform.position.y - startPos.y);
-        float bgMoveX = (distMovedX * bgShift);
-        float bgMoveY = (distMovedY * bgShift);
+        float bgMoveX = (distMovedX * bgHorizShift);
+        float bgMoveY = (distMovedY * bgVertShift);
 
-        Vector3 newPos = new Vector3(Camera.main.transform.position.x + bgMoveX - (startPos.x - bgStartPos.x), Camera.main.transform.position.y + bgMoveY - (startPos.y - bgStartPos.y), transform.position.z);
+        Vector3 newPos = new Vector3(Camera.main.transform.position.x + bgMoveX - (startPos.x - bgStartPos.x), Camera.main.transform.position.y - bgMoveY - (startPos.y - bgStartPos.y), transform.position.z);
         transform.position = newPos;
     }
 }
