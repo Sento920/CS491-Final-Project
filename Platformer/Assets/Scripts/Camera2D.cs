7db72ed6e0ec5d5c521daddcf68d9bded955fc9e
uwecrayat <rayat@uwec.edu>
2015-12-15 14:16:00 -0600

    Added a wait time for shifting the camera when changing directions

--- Platformer/Assets/Scripts/Camera2D.cs
+++ Platformer/Assets/Scripts/Camera2D.cs
@@ -9,16 +9,20 @@ using UnityEngine;
 	 public float vertShift;
      private Vector3 velocity = Vector3.zero;
      public Transform target;
- 
+	private float turnTime;
+
      // Update is called once per frame
      void Update () 
      {
          if (target)
          {
-			if (target.localScale.x == -1) {
-				horizShift = 0.7f;
-			} else {
-				horizShift = 0.3f;
+			if (Time.time > turnTime) {
+				turnTime = Time.time + 0.75f;
+				if (target.localScale.x == -1) {
+					horizShift = 0.7f;
+				} else {
+					horizShift = 0.3f;
+				}
 			}
              Vector3 point = GetComponent<Camera>().WorldToViewportPoint(target.position);
 			Vector3 delta = target.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(horizShift, vertShift, point.z)); //(new Vector3(0.5, 0.5, point.z));
