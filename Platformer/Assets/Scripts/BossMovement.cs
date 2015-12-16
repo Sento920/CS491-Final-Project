eb850c414cd8cba977131a2edf6a872878815f32
uwecrayat <rayat@uwec.edu>
2015-12-16 16:09:54 -0600

    boss stuff

--- Platformer/Assets/Scripts/BossMovement.cs
+++ Platformer/Assets/Scripts/BossMovement.cs
@@ -4,10 +4,15 @@ using System.Collections;
 public class BossMovement : MonoBehaviour {
     //X: [10, 40]
     //Y: [58, 64]
+    private ChildController childController;
+    public GameObject projectile;
+    public GameObject player;
+    private Vector3 playerPos;
     private float waitTime;
+    private float bossWait;
 	// Use this for initialization
 	void Start () {
-	
+        childController = GetComponent<ChildController>();
 	}
 	
 	// Update is called once per frame
@@ -17,6 +22,26 @@ public class BossMovement : MonoBehaviour {
             waitTime = Time.time + 2f;
             Vector3 newPos = new Vector3(UnityEngine.Random.Range(10, 40), UnityEngine.Random.Range(58, 64), 1);
             transform.position = Vector3.Slerp(transform.position, newPos, Time.time);
+        }
+        if (childController.getChildCount() == 0)
+        {
+            if (Time.time > bossWait)
+            {
+                print("child count = 0");
+                bossWait = Time.time + 1f;
+                {
+                    print("firing");
+                    playerPos = player.transform.position;
+                   
+                        float angle = Mathf.Atan2(transform.position.y - playerPos.y, transform.position.x - playerPos.x) * Mathf.Rad2Deg;
+
+                        GameObject tmp = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+                        tmp.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+                        tmp.GetComponent<Rigidbody2D>().velocity = (playerPos - transform.position).normalized * 20;
+                        
+                    
+                }
+            }
         }
 	}
 }
