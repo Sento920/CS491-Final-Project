193dc0e57aa891f5548d2b0c85852be20ce40868
uwecrayat <rayat@uwec.edu>
2015-12-15 19:10:08 -0600

    level transport implemented

---
+++ Platformer/Assets/Scripts/LevelTransport.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelTransport : MonoBehaviour {
+	public string lvlToLoad;
+	
+	void OnTriggerEnter2D(Collider2D coll) {
+		Application.LoadLevel(lvlToLoad);
+	}
+}
